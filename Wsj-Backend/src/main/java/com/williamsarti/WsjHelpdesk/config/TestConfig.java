98291843d8203ca3cabd7beb8eb8adb6ffca951d
williamsartijose <williamsartijose@hotmail.com>
2022-03-01 16:22:05 -0300

    [Back-End]-Java API Rest - spring boot - Perfil de teste criado

---
+++ Wsj-Backend/src/main/java/com/williamsarti/WsjHelpdesk/config/TestConfig.java
@@ -0,0 +1,21 @@
+package com.williamsarti.WsjHelpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.williamsarti.WsjHelpdesk.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public void instanciaDB() {
+		this.dbService.instanciaDB();
+	}
+}
