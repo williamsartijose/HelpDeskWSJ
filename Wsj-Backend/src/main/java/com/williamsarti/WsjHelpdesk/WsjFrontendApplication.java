72370add7af6853677980d2f11c93b8d8dc4a9f3
williamsartijose <williamsartijose@hotmail.com>
2022-03-01 14:17:28 -0300

    [Back-End]-Java API Rest - spring boot - Iniciando o Projeto

---
+++ Wsj-Backend/src/main/java/com/williamsarti/WsjHelpdesk/WsjFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.williamsarti.WsjHelpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsjFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsjFrontendApplication.class, args);
+	}
+
+}
---
+++ Wsj-Backend/src/test/java/com/williamsarti/WsjHelpdesk/WsjFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williamsarti.WsjHelpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsjFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
