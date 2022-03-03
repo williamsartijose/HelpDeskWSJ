7fe3a4e10a04942b67c64f6a722d9236404c2a4f
williamsartijose <williamsartijose@hotmail.com>
2022-03-03 11:35:57 -0300

    [Front-End]- Angular - VisualCode - Componente Login criado

--- Wsj-Frontend/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ Wsj-Frontend/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
+
 
 @Component({
   selector: 'app-tecnico-list',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoListComponent implements OnInit {
 
+  ELEMENT_DATA: Tecnico[] = [
+
+    {
+      id:1,
+      nome: 'william sarti',
+      cpf: '123.456.789.-10',
+      email: 'williamsarti@gmail.com',
+      senha: '1234',
+      perfis: ['0'],
+      dataCriacao: '15/08/2022'
+    }
+  ]
+
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+  
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
+
