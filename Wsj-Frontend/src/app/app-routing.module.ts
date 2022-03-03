c04edd3e0215180429d7de3fb60849b41e0748be
williamsartijose <williamsartijose@hotmail.com>
2022-03-03 10:23:17 -0300

    [Front-End]- Angular - VisualCode - Rota para TECNICO-LIST

--- Wsj-Frontend/src/app/app-routing.module.ts
+++ Wsj-Frontend/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 const routes: Routes = [
 {
-  path: '', component: NavComponent, children: [{path: 'home', component: HomeComponent}]
+  path: '', component: NavComponent, children: [
+    {path: 'home', component: HomeComponent},
+   { path: 'tecnicos', component: TecnicoListComponent }
+  ]
 }
 ];
 
