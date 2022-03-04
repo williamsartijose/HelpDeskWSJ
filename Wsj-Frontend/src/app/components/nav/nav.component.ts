598b8b5f3e3965c8789844b0fef5d2ce26950d37
williamsartijose <williamsartijose@hotmail.com>
2022-03-04 14:52:15 -0300

    [Front-End]- Angular - VisualCode - Autenticação de logout

--- Wsj-Frontend/src/app/components/nav/nav.component.ts
+++ Wsj-Frontend/src/app/components/nav/nav.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/service/auth.service';
+
 
 @Component({
   selector: 'app-nav',
@@ -8,10 +11,18 @@ import { Router } from '@angular/router';
 })
 export class NavComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private authService:AuthService,
+    private toast: ToastrService) { }
 
   ngOnInit(): void {
     this.router.navigate(['home'])
   }
 
+  logout() {
+    this.router.navigate(['login'])
+    this.authService.logout();
+    this.toast.info('Logout realizado com sucesso', 'Logout')
+  }
 }
