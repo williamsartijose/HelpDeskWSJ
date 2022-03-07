c233109c494f166ff59cff6dec260227124a3c4b
williamsartijose <williamsartijose@hotmail.com>
2022-03-07 08:29:48 -0300

    [Front-End]- Angular - VisualCode - Componente Tecnico Update implementado

--- Wsj-Frontend/src/app/app-routing.module.ts
+++ Wsj-Frontend/src/app/app-routing.module.ts
@@ -6,14 +6,18 @@ import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
 import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
+import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
 {
   path: '', component: NavComponent,canActivate: [AuthGuard] , children: [
     {path: 'home', component: HomeComponent},
+
+    { path: 'tecnicos', component: TecnicoListComponent },
     { path: 'tecnicos/create',     component: TecnicoCreateComponent },
-   { path: 'tecnicos', component: TecnicoListComponent }
+    { path: 'tecnicos/update/:id', component: TecnicoUpdateComponent }
+  
   ]
 }
 ];
