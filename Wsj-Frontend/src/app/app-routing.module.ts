a76ffb42fed0df40ad0fa4ebca0dc2c9f04ad653
williamsartijose <williamsartijose@hotmail.com>
2022-03-07 08:46:09 -0300

    [Front-End]- Angular - VisualCode - Componente Tecnico Delete implementado

--- Wsj-Frontend/src/app/app-routing.module.ts
+++ Wsj-Frontend/src/app/app-routing.module.ts
@@ -5,21 +5,22 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
 import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
+import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecnico-delete.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-{
-  path: '', component: NavComponent,canActivate: [AuthGuard] , children: [
-    {path: 'home', component: HomeComponent},
+  {
+    path: '', component: NavComponent, canActivate: [AuthGuard], children: [
+      { path: 'home', component: HomeComponent },
 
-    { path: 'tecnicos', component: TecnicoListComponent },
-    { path: 'tecnicos/create',     component: TecnicoCreateComponent },
-    { path: 'tecnicos/update/:id', component: TecnicoUpdateComponent }
-  
-  ]
-}
+      { path: 'tecnicos',            component:   TecnicoListComponent },
+      { path: 'tecnicos/create',     component: TecnicoCreateComponent },
+      { path: 'tecnicos/update/:id', component: TecnicoUpdateComponent },
+      { path: 'tecnicos/delete/:id', component: TecnicoDeleteComponent },
+    ]
+  }
 ];
 
 @NgModule({
--- Wsj-Frontend/src/app/app.module.ts
+++ Wsj-Frontend/src/app/app.module.ts
@@ -37,6 +37,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
 import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
+import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecnico-delete.component';
 
 @NgModule({
   declarations: [
@@ -47,7 +48,8 @@ import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecn
     TecnicoListComponent,
     LoginComponent,
     TecnicoCreateComponent,
-    TecnicoUpdateComponent
+    TecnicoUpdateComponent,
+    TecnicoDeleteComponent
   ],
   imports: [
     BrowserModule,
